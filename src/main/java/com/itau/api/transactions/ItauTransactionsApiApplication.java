15913bd8ec0e8d69b1cfe1dde32d5f41fcd54db1
Mel Saraiva <melcr306@gmail.com>
2025-09-24 12:50:21 -0300

    Initial commit

---
+++ src/main/java/com/itau/api/transactions/ItauTransactionsApiApplication.java
@@ -0,0 +1,13 @@
+package com.itau.api.transactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauTransactionsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItauTransactionsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itau/api/transactions/ItauTransactionsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itau.api.transactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauTransactionsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
